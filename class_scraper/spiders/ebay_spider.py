eebad1dacef6c34eb45d642cf7cfa2d6f3a0458a
Zach Her <60132730+dankbudgie@users.noreply.github.com>
2022-04-06 13:54:48 -0700

    ebay cleaned up, newegg needs next page logic

--- class_scraper/spiders/ebay_spider.py
+++ class_scraper/spiders/ebay_spider.py
@@ -2,14 +2,17 @@ import scrapy
 
 class EbaySpider(scrapy.Spider):
     name = 'ebay'
-    start_urls = ['https://www.ebay.com/sch/i.html?_nkw=computer&ssPageName=GSTL']
+    start_urls = ['https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw=graphics+cards&_sacat=0&LH_TitleDesc=0&rt=nc&_odkw=computer&ssPageName=GSTL']
 
     def parse(self, response):
-        for products in response.css('div[data-content="productItem"]'):
-            yield {
-                'name': products.css('h3.s-item__title::text').get(),
-                'price': products.css('span.s-item__price::text').get().replace('$', ''),
-            }
+        try:
+            for products in response.css('div.s-item__wrapper.clearfix'):
+                yield {
+                    'name': products.css('h3.s-item__title::text').get(),
+                    'price': products.css('span.s-item__price::text').get().replace('$', ''),
+                }
+        except:
+            pass
         
         next_page = response.css('a.pagination__next.icon-link').attrib['href']
         if next_page is not None:
